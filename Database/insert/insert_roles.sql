afca37ebb5c2b92e76e56cb735ae39c9820ce0e8
Atanas <a.arshinkov97@gmail.com>
2018-11-14 01:32:32 +0200

    Removed database configuration from repository

--- Database/insert/insert_roles.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO ROLES
-VALUES
-  ('ADMIN', 'Administrator', 'Administrator of website');
-
-INSERT INTO ROLES VALUES ('USER', 'User', 'Normal user');
-
-INSERT INTO ROLES
-VALUES
-  ('DBA', 'Database admin', 'Administrator of the database');
--- Database/sequences/s_users.sql
+++
@@ -1,5 +0,0 @@
--- Create sequence 
-create sequence S_USERS
-start with 1
-increment by 1
-nocache;
