295ac28f923959c41accb5914f47c8b24a82806e
Atanas Arshinkov <a.arshinkov97@gmail.com>
2018-11-16 00:58:00 +0200

    Story design

--- StoryCom/src/main/java/com/storycom/controllers/TestController.java
+++ StoryCom/src/main/java/com/storycom/controllers/TestController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,6 +24,13 @@ public class TestController extends Base {
 
     @Autowired
     private StoriesRepository storiesRepository;
+
+    @GetMapping(value = "/design")
+    public String designTest() {
+        log.debug("Design test");
+
+        return "test/designTest";
+    }
 
     @ResponseBody
     @RequestMapping(value = "/story/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
