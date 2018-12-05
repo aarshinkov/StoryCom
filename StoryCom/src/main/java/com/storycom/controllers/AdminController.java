bc686777610e4700282bbd38e84e2ff79378ed69
Atanas Arshinkov <a.arshinkov97@gmail.com>
2018-12-05 16:21:59 +0200

    Made admin user section, welcome message and some minor bug fixes

---
+++ StoryCom/src/main/java/com/storycom/controllers/AdminController.java
@@ -0,0 +1,30 @@
+package com.storycom.controllers;
+
+import com.storycom.entity.User;
+import com.storycom.repository.UsersRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/admin")
+public class AdminController {
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @GetMapping(value = "/users")
+    public String users(Model model) {
+        List<User> users = usersRepository.findAllByOrderByUserIdDesc();
+
+        model.addAttribute("users", users);
+        return "admin/users/users";
+    }
+}
--- StoryCom/src/main/java/com/storycom/repository/UsersRepository.java
+++ StoryCom/src/main/java/com/storycom/repository/UsersRepository.java
@@ -3,7 +3,10 @@ package com.storycom.repository;
 import com.storycom.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UsersRepository extends JpaRepository<User, Integer> {
+    List<User> findAllByOrderByUserIdDesc();
     User findUserByUsername(String username);
     User findByUserId(Integer userId);
 }
