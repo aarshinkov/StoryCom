8928823f2a2756787dd9270e63cc3b73a083259f
Atanas Arshinkov <a.arshinkov97@gmail.com>
2019-09-09 17:13:33 +0300

    Added first test

---
+++ StoryCom/src/test/java/com/storycom/test/controllers/HomeControllerTest.java
@@ -0,0 +1,36 @@
+package com.storycom.test.controllers;
+
+import com.storycom.controllers.*;
+import org.junit.*;
+import org.springframework.test.web.servlet.*;
+import org.springframework.test.web.servlet.setup.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import org.springframework.test.web.servlet.result.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class HomeControllerTest
+{
+
+  private MockMvc mockMvc;
+
+  @Before
+  public void setup()
+  {
+    this.mockMvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
+  }
+
+  @Test
+  public void testHomePage() throws Exception
+  {
+    this.mockMvc.perform(get(""))
+            .andExpect(status().isOk())
+            .andExpect(view().name("home/home"))
+            .andExpect(model().attribute("submenu", "home"))
+            .andDo(MockMvcResultHandlers.print())
+            .andReturn();
+  }
+
+}
